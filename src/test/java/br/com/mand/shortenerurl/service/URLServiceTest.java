ca3d75074be0f4c3f28665962a9261d080777081
Adriano Junior <mand458@hotmail.com>
2022-07-03 23:20:57 -0300

    Adiciona dois testes

--- src/test/java/br/com/mand/shortenerurl/service/URLServiceTest.java
+++ src/test/java/br/com/mand/shortenerurl/service/URLServiceTest.java
@@ -1,12 +1,21 @@
 package br.com.mand.shortenerurl.service;
 
+import br.com.mand.shortenerurl.url.entity.ShortenUrl;
 import br.com.mand.shortenerurl.url.repository.URLRepository;
+import br.com.mand.shortenerurl.url.response.ShortenUrlResponse;
 import br.com.mand.shortenerurl.url.service.URLService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+
+@ExtendWith(MockitoExtension.class)
 public class URLServiceTest {
 
 
@@ -17,8 +26,20 @@ public class URLServiceTest {
     URLRepository urlRepository;
 
     @Test
-    public void should(){
-        Mockito.when(urlRepository.findByShortUrl(null)).thenReturn(null);
+    public void shouldShortUrlWithMaxSizeTen(){
+        ShortenUrl shortenUrl =new ShortenUrl();
+        shortenUrl.setUrlRedirect("https://g1.globo.com/");
+        Mockito.when(urlRepository.save(any())).thenReturn(shortenUrl);
+        ShortenUrlResponse shortenUrlResponse = urlService.shortUrl(shortenUrl);
+        assertTrue(shortenUrlResponse.getNewUrl().split("/")[3].length() >= 5 && shortenUrlResponse.getNewUrl().split("/")[3].length() <= 10 );
+        assertFalse(shortenUrlResponse.getNewUrl().split("/")[3].matches("[^a-zA-Z0-9_]+"));
+    }
+
+    @Test
+    public void shouldThrowRuntimeExceptionWhenNotFound(){
+        assertThrows(RuntimeException.class, ()->{
+            urlService.findByUrlShort("DSADSA");
+        });
     }
 
 }
